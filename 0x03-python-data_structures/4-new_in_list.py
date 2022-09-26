903665172876ba52fcee05742146ba1d61b5d1ef
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-09-26 14:35:47 -0700

    replace list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy_elem = my_list[:]
+
+    if idx < 0 or idx >= len(copy_elem):
+            return copy_elem
+        copy_elem = element
+        return copy_elem
