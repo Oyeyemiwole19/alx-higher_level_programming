ab8f5b108cbbaad67b879c96992dc59fced84dd4
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-09-26 14:44:22 -0700

    repalce list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    copy_elem = my_list[:]
-
-    if idx < 0 or idx >= len(copy_elem):
-            return copy_elem
-        copy_elem = element
-        return copy_elem
+    copy = my_list[:]
+    if idx < 0 or idx >= len(copy):
+        return copy
+    copy[idx] = element
+    return copy
