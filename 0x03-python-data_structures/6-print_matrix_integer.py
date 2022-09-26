b7dff7a103c699145b6a90a6cbb83792007b3951
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-09-26 16:12:39 -0700

    task 6 update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,6 @@ def print_matrix_integer(matrix=[[]]):
         print(format_i.format(*i))
 
 
-
 if __name__ == '__main__':
     mat = [[a, a+1, a+2] for a in range(1, 16, 3)]
     print_matrix_integer(mat)
