b22ceafe6bc59ee061c2391986eacfd7ba6ab0fa
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-09-26 16:34:47 -0700

    taask

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 def add_tuple(a=(), b=()):
-while len(a) < 2:
-a = (*a, 0)
-while len(b) < 2:
-b = (*b, 0)
+    while len(a) < 2:
+        a = (*a, 0)
+
+    while len(b) < 2:
+        b = (*b, 0)
+
+
 return a[0] + b[0], a[1] + b[1]
 
 
