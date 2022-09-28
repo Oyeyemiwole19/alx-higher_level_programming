fc6735cf44264f40faad1a8315e7f9644ccfcd33
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-09-28 15:35:39 -0700

    task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    if a_dictionary is None:
+        return None
+    return {
+            key: a_dictionary[key] * 2 for key in a_dictionary
+            }
